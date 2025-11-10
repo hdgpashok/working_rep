fe4c6dc5e302f66c9751057d6c81d30e153d9bca
Pavel <bobrov_71@list.ru>
2025-11-10 14:43:15 +0300

    some fixes & db init

--- src/one_to_one/models.py
+++ src/one_to_one/models.py
@@ -1,8 +1,11 @@
+import asyncio
 import uuid
 
 import sqlalchemy as sa
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeMeta, Mapped, declarative_base, mapped_column, relationship
+
+from db import engine
 
 metadata = sa.MetaData()
 
@@ -21,11 +24,11 @@ Base: DeclarativeMeta = declarative_base(metadata=metadata, cls=BaseServiceModel
 class UserModel(Base):
     __tablename__ = 'user'
 
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, autoincrement=True, default=uuid.uuid4())
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4())
     title: Mapped[str] = mapped_column(sa.String())
 
     profile: Mapped['ProfileModel'] = relationship(
-        "Profile",
+        "ProfileModel",
         back_populates='user',
         uselist=False,
         cascade='all, delete-orphan'
@@ -35,9 +38,22 @@ class UserModel(Base):
 class ProfileModel(Base):
     __tablename__ = 'profile'
 
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, autoincrement=True, default=uuid.uuid4())
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4())
     title: Mapped[str] = mapped_column(sa.String())
     bio: Mapped[str] = mapped_column(sa.String())
     user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"))
 
-    user: Mapped["UserModel"] = relationship('user', back_populates='profile')
+    user: Mapped["UserModel"] = relationship('UserModel', back_populates='profile')
+
+
+async def create_database_tables():
+    try:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+        print("success init")
+    except Exception as e:
+        print(f"error: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(create_database_tables())
