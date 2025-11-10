38b87c481b3dea408bcd633bfab50ebe239de106
Pavel <bobrov_71@list.ru>
2025-11-10 14:41:27 +0300

    create user in db func

--- src/one_to_one/crud.py
+++ src/one_to_one/crud.py
@@ -1,17 +1,9 @@
-from typing import Annotated
-
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.db import get_session
+from src.db import SessionDep
 from src.one_to_one.models import UserModel, ProfileModel
-from src.one_to_one.schemas import UserCreate, ProfileCreate
+from src.one_to_one.schemas import UserCreate
 
 
-SessionDep = Annotated[AsyncSession, Depends(get_session)]
-
-
-async def create_user(user: UserCreate, session: SessionDep) -> None:
+async def create_user_db(user: UserCreate, session: SessionDep) -> None:
     new_profile = ProfileModel(
         **user.profile.model_dump()
     )
