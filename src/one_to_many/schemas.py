23eb404d18e38ca3088cc6c1b6af7c7293fcb107
Pavel <bobrov_71@list.ru>
2025-11-11 17:34:13 +0300

    new schemas

--- src/one_to_many/schemas.py
+++ src/one_to_many/schemas.py
@@ -8,8 +8,21 @@ class BookCreate(BaseModel):
     title: str
 
 
+class BookOut(BaseModel):
+    title: str
+    author_id: UUID
+
+
 class AuthorCreate(BaseModel):
     first_name: str
     last_name: str
 
     books: Optional[list[BookCreate]]
+
+
+class AuthorRead(BaseModel):
+    id: UUID
+
+
+class AuthorOut(AuthorCreate):
+    id: UUID
