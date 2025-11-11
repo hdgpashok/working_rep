db2f39b96b5364fcfae84af884d1e4a19d39a305
Pavel <bobrov_71@list.ru>
2025-11-11 17:34:23 +0300

    get endpoint added

--- src/one_to_many/endpoints.py
+++ src/one_to_many/endpoints.py
@@ -1,11 +1,12 @@
+from uuid import UUID
+
 from fastapi import APIRouter
 
 from src.db import SessionDep
 
-from src.one_to_many.schemas import AuthorCreate
+from src.one_to_many.schemas import AuthorCreate, AuthorRead
 
-from src.one_to_many.crud import create_author_in_db
-
+from src.one_to_many.crud import create_author_in_db, get_author_in_db
 
 router = APIRouter()
 
@@ -14,3 +15,10 @@ router = APIRouter()
 async def create_author(author: AuthorCreate, session: SessionDep):
     await create_author_in_db(author, session)
     return {'status': 'user created'}
+
+
+@router.get("/get_author/{author_id}")
+async def get_author(author_id: UUID, session: SessionDep):
+    result = await get_author_in_db(author_id, session)
+
+    return result
